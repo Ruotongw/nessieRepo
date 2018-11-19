ed81e9cff0e3096351db7ab17bcd89448f96fcf0
jcrayton <jcrayton@macalester.edu>
2018-11-19 08:51:44 -0600

    fixed the redirect bug

--- app/views.py
+++ app/views.py
@@ -58,6 +58,7 @@ def main():
 @app.route('/form', methods=['GET', 'POST']) #allow both GET and POST requests
 def form():
     try:
+        print (service)
         if request.method == 'POST': #this block is only entered when the form is submitted
             if not request.headers.get('X-Requested-With'):
 

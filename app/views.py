2085da8e87a8c670b53c1e4a2ebb21888e839413
Sophie Pincus-Kazmar <spincusk@macalester.edu>
2018-12-07 15:12:05 -0600

    streamlined daylight savings checks

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ from tzlocal import get_localzone
 import calendar
 from .format import *
 from .timeSlot import *
-from .now import *
+from .time import *
 from .findTime import *
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
@@ -154,122 +154,6 @@ def getEvents():
     return eventsJSON
 
 
-# def findAvailableTimes(nowDay, nowHour, nowMinute, workStart, workEnd, events, timeEst):
-#     '''Calculates every available time slot in relation to the events on the user's
-#     calender from now until the assignment due date. Returns a list of these time slots.'''
-#
-#     global format
-#     format = Format()
-#     global timeSlot
-#     timeSlot = TimeSlot(timeEst)
-#     # global availableTimes
-#     # availableTimes = []
-#
-#     for i in range(len(events) - 1):
-#
-#         event1 = events[i]
-#         event2 = events[i + 1]
-#         e1, e2 = format.formatEvent(event1, event2)
-#         compareEvents(e1, e2, workStart, workEnd, nowDay, nowHour, nowMinute, timeEst)
-#
-#     lastEvent = events[len(events) - 1]
-#     secondToLast = events[len(events) - 2]
-#     compareLastEvent(lastEvent, secondToLast, workStart, workEnd, nowDay, nowHour, nowMinute, timeEst)
-#
-#     # print(availableTimes)
-#     return availableTimes
-#
-#
-# def compareLastEvent(lastEvent, secondToLast, workStart, workEnd, nowDay, nowHour, nowMinute, timeEst):
-#     '''Accounts for finding the time slots around the the last event before the
-#     deadline. Also accounts for if there is only one event before the deadline.'''
-#
-#     estTimeMin = timeEst * 60
-#     estMins = estTimeMin % 60
-#     estHours = (estTimeMin - estMins) / 60
-#     openHours, openMinutes = openTimeWindow(workStart, workEnd)
-#
-#     lastEnd, lastStart = format.formatEvent(lastEvent, lastEvent)
-#     secondEnd, secondStart = format.formatEvent(secondToLast, secondToLast)
-#
-#     timeWindow = (lastEnd.hour * 60) + lastEnd.minute + (estTimeMin + 30)
-#
-#     beforeTime = (lastStart.hour * 60 + lastStart.minute) - (workStart*60)
-#     enoughBeforeTime = (beforeTime >= estTimeMin + 30)
-#
-#     timeDiff = (lastStart.hour * 60 + lastStart.minute) - (nowHour * 60 + nowMinute)
-#     enoughTime = (timeDiff >= (estTimeMin + 30))
-#
-#     timeDiffEvent = (lastStart.hour * 60 + lastStart.minute) - (secondEnd.hour * 60 + secondEnd.minute)
-#     enoughTimeEvent = (timeDiffEvent >= (estTimeMin + 30))
-#
-#     diffDays = lastStart.day != nowDay
-#     diffEventDays = lastStart.day != secondEnd.day
-#
-#     if(enoughBeforeTime and (enoughTime or diffDays) and (diffEventDays or enoughTimeEvent)):
-#         time = timeSlot.beforeTimeSlot(lastStart)
-#         availableTimes.append(time)
-#
-#     if((lastEnd.hour in openHours) and (timeWindow in openMinutes)):
-#         time = timeSlot.afterTimeSlot(lastEnd)
-#         availableTimes.append(time)
-#
-#
-# def compareEvents(e1, e2, workStart, workEnd, nowDay, nowHour, nowMinute, timeEst):
-#     '''Compares each pair of events on the user's calendar from now until the
-#     entered deadline. If there is enough time between the events, the time slot
-#     between them is added to the list of available times.'''
-#
-#     # global availableTimes
-#     # availableTimes = []
-#
-#     estTimeMin = timeEst * 60
-#     estMins = estTimeMin % 60
-#     estHours = (estTimeMin - estMins) / 60
-#     openHours, openMinutes = openTimeWindow(workStart, workEnd)
-#
-#     sameDay = (e1.day == e2.day)
-#
-#     #for time in morning before eventTime
-#     morningTime = (e2.hour * 60 + e2.minute) - (workStart*60)
-#     enoughMorningTime = morningTime >= estTimeMin + 30
-#
-#     # For events on the same day
-#     timeDiff = (e2.hour * 60 + e2.minute) - (e1.hour * 60 + e1.minute)
-#     enoughTime = timeDiff >= (estTimeMin + 30)
-#
-#     # For events on different days
-#     timeDiff2 = (1440 - (e1.hour * 60) + e1.minute) + (e2.hour * 60 + e2.minute)
-#     enoughTime2 = timeDiff2 >= (estTimeMin + 30)
-#
-#     # Ensures that the algorithm doesn't schedule events in the past
-#     now = ((e1.hour == nowHour) and (e1.minute >= nowMinute)) or e1.hour > nowHour or (e1.day > nowDay)
-#
-#     # Ensures that the entire scheduled event would be within the open working hours
-#     timeWindow = (e1.hour * 60) + e1.minute + (estTimeMin + 30)
-#
-#     if(now and (sameDay and enoughTime and (e1.hour in openHours) and (timeWindow in openMinutes))):
-#         time = timeSlot.afterTimeSlot(e1)
-#         availableTimes.append(time)
-#
-#     if(now and (not sameDay and enoughTime2 and (e1.hour in openHours) and (timeWindow in openMinutes))):
-#         time = timeSlot.afterTimeSlot(e1)
-#         availableTimes.append(time)
-#
-#     if(not sameDay and enoughMorningTime):
-#         time = timeSlot.beforeTimeSlot(e2)
-#         availableTimes.append(time)
-#
-# def openTimeWindow(openStartTime, openEndTime):
-#     '''Returns the time during the day when the user can work on assignments
-#     in terms of hours and in terms of the minutes out of the entire minutes
-#     in a day.'''
-#
-#     openHours = range(openStartTime, openEndTime)
-#     openMinutes = range(openStartTime * 60, openEndTime * 60)
-#     return openHours, openMinutes
-
-
 def getCalendarEvents(min, max):
     '''Returns a list with every event on the user's primary Google Calendar
     from now unti the due date in cronological order. Each event is a dictionary.'''
@@ -348,7 +232,7 @@ def createEvent():
     and prepares it to be added to the user's calendar.'''
 
     global current
-    current = Now()
+    current = Time()
 
     global format
     format = Format()

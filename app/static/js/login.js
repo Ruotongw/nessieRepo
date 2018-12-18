a642dd7eb76075e1569bc12f977e6b826a629e6a
ruotongw <rwang2@macalester.edu>
2018-12-17 20:12:22 -0600

    chat layout for landing screen

---
+++ app/static/js/login.js
@@ -0,0 +1,20 @@
+console.log("easing");
+$("li:nth-child(1)").show("drop", 100, function() {
+$("li:nth-child(2)").delay(100).show("drop", 100, function() {
+$("li:nth-child(3)").delay(100).show("drop", 100, function() {
+$("li:nth-child(4)").delay(100).show("drop", 100, function() {
+$("li:nth-child(5)").delay(100).show("drop", 100, function() {
+}); 
+});
+}); 
+});
+}); 
+
+
+// https://codepen.io/rockkk/pen/JKjMdJ
+
+
+
+
+
+
